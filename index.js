c64bd8dd5ed4ebdba4b41f3a06c9c964bbac6b56
einzbern <einzberne@gmail.com>
2021-12-29 00:57:03 +0700

    add postRoute & userRoute

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer")
+const path = require("path");
 
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -21,7 +25,30 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/api/auth", authRoute)
+app.use("/images/person", express.static(path.join(__dirname,"public/images/person")));
+app.use("/images/post", express.static(path.join(__dirname,"public/images/post")));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images/post");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+      return res.status(200).json("File uploded successfully");
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8900, () => {
     console.log('Backend server is running')
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const { resetWatchers } = require('nodemon/lib/monitor/watch');
 //register
 router.post("/register", async (req, res) => {
     try {
+        console.log("register")
         //generate hashed password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
