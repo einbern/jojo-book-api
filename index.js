d5f9ecad6548306521e540eda661b38730dca782
einzbern <einzberne@gmail.com>
2022-01-01 16:43:15 +0700

    setting cors

--- index.js
+++ index.js
@@ -12,46 +12,40 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const uploadRoute = require("./routes/upload");
+const { json } = require("express/lib/response");
 
 dotenv.config();
 
-mongoose 
- .connect(process.env.MONGO_URL, {
+app.get("/", (req, res) => {
+    res.status(200).json('app work')
+})
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
+
+app.listen(PORT, () => {
+    console.log(`Backend Serer is running. ${PORT}`)
+})
+
+mongoose
+    .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
-        useUnifiedTopology: true})   
- .then(() => console.log("Connected to MongoDB!!"))
- .catch(err => console.log(err));
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Connected to MongoDB!!"))
+    .catch(err => console.log(err));
 
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/images", express.static(path.join(__dirname,"public/images")));
-
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//       cb(null, "public/images/post");
-//     },
-//     filename: (req, file, cb) => {
-//       cb(null, req.body.name);
-//     },
-//   });
-  
-//   const upload = multer({ storage: storage });
-//   app.post("/api/upload", upload.single("file"), (req, res) => {
-//     try {
-//       return res.status(200).json("File uploded successfully");
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   });
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/upload", uploadRoute);
-
-app.listen(PORT, () => {
-    console.log(`Backend Serer is running. ${PORT}`)
-})
